d564325e1d40501efe548efc993782ed94b31a3b
Maiamba <mayambalive1999@outlook.com>
2021-01-12 16:58:15 +0100

    12-01-2021

---
+++ WebApiAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+
+
+
+
+//HttpContext httpContext = HttpContext.Current;
+
+//string authHeader = this.httpContext.Request.Headers["Authorization"];
+
+//if (authHeader != null && authHeader.StartsWith("Basic")) {
+//    string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+//Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+//string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+//int seperatorIndex = usernamePassword.IndexOf(':');
+
+//var username = usernamePassword.Substring(0, seperatorIndex);
+//var password = usernamePassword.Substring(seperatorIndex + 1);
+//} else {
+//    //Handle what happens if that isn't the case
+//    throw new Exception("The authorization header is either empty or isn't Basic.");
+//}
---
+++ WebApiAuthentication/Filters/AuthFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiMiddleware.Filters
+{
+    public class AuthFilter : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var json = new
+            {
+                context.Response.StatusCode,
+                Message = "An error occurred whilst processing your request",
+                Detailed = exception
+            };
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(json));
+        }
+    }
+
+}
